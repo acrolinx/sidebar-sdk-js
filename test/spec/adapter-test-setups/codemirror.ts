ad924c6aabec46811998bfb8a854e0dc1761697b
marco <Marco.Stahl@acrolinx.com>
2017-12-06 18:49:47 +0100

    Fixed ts declarations build problem

--- test/spec/adapter-test-setups/codemirror.ts
+++ test/spec/adapter-test-setups/codemirror.ts
@@ -1,11 +1,12 @@
 import {AdapterTestSetup, DoneCallback, InitAdapterCallback} from "./adapter-test-setup";
 import {CodeMirrorAdapter} from "../../../src/adapters/CodeMirrorAdapter";
+import {EditorFromTextArea} from "codemirror";
 
 export class CodeMirrorTestSetup implements AdapterTestSetup {
   name = 'CodeMirrorAdapter';
   inputFormat = 'TEXT';
   editorElement = '<textarea id="editorId">initial text</textarea>';
-  editor: CodeMirror.EditorFromTextArea;
+  editor: EditorFromTextArea;
 
   init(done: InitAdapterCallback) {
     const editor = CodeMirror.fromTextArea(document.getElementById('editorId') as HTMLTextAreaElement, {
