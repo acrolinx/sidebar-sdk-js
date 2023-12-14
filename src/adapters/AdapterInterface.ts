18e1b5651eeb15aaacafcadd4b447621dcc7c2e0
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2023-12-14 18:18:59 +0100

    DEV-36849; fix: lint

--- src/adapters/AdapterInterface.ts
+++ src/adapters/AdapterInterface.ts
@@ -79,7 +79,7 @@ export interface CommonAdapterInterface {
   registerCheckCall(checkInfo: Check): void;
   registerCheckResult(checkResult: SuccessfulCheckResult): void;
   getAutobindWrapperAttributes?(): AutobindWrapperAttributes;
-  onInitFinished?(result: InitResult):  void;
+  onInitFinished?(result: InitResult): void;
 }
 
 export interface AdapterInterface extends CommonAdapterInterface {
