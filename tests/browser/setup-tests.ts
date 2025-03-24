ed5487148363f7c45eb8500ea7f112e57f2a68f5
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2025-03-24 08:47:35 +0100

    refactor

--- tests/browser/setup-tests.ts
+++ tests/browser/setup-tests.ts
@@ -1,3 +1,3 @@
 import { vi } from 'vitest';
 
-vi.stubGlobal('SDK_VERSION', '9.9.9'); 
+vi.stubGlobal('SDK_VERSION', '9.9.9');
--- tests/browser/utils/util.ts
+++ tests/browser/utils/util.ts
@@ -1,5 +1,69 @@
+import { expect } from 'vitest';
+import { AdapterInterface, SuccessfulContentExtractionResult } from '../../../src/adapters/adapter-interface';
+import { AdapterTestSetup } from '../adapter-setups/adapter-setup';
+
 export function htmlStringToElements(htmlString: string) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(htmlString, 'text/html');
   return doc.body.firstElementChild as Element;
 }
+
+export const assertEditorRawContent = (adapter: AdapterInterface, expectedContent: string) => {
+  const editorContent = (adapter.extractContentForCheck({}) as SuccessfulContentExtractionResult).content;
+  expect(editorContent).toEqual(expectedContent);
+};
+
+export const assertEditorText = (adapterSpec: AdapterTestSetup, adapter: AdapterInterface, expectedText: string) => {
+  console.log(expectedText);
+  const editorContent = (adapter.extractContentForCheck({}) as SuccessfulContentExtractionResult).content;
+  if (adapterSpec.inputFormat === 'TEXT') {
+    expect(editorContent).toEqual(expectedText);
+  } else {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = editorContent;
+    const actualText = tempDiv.textContent!.replace('\n', '');
+    expect(actualText).toEqual(expectedText);
+  }
+};
+
+export const registerCheckResult = (adapter: AdapterInterface, text: string, dummyCheckId: string) => {
+  adapter.registerCheckResult({
+    checkedPart: {
+      checkId: dummyCheckId,
+      range: [0, text.length],
+    },
+  });
+};
+
+export const givenAText = (
+  adapter: AdapterInterface,
+  adapterSpec: AdapterTestSetup,
+  dummyCheckId: string,
+  text: string,
+  callback: (initialExtractedContent: string) => void,
+) => {
+  adapterSpec.setEditorContent(text);
+
+  console.log('callback called');
+  adapter.registerCheckCall({ checkId: dummyCheckId });
+  const contentExtractionResult = adapter.extractContentForCheck({}) as SuccessfulContentExtractionResult;
+  registerCheckResult(adapter, text, dummyCheckId);
+  callback(contentExtractionResult.content);
+};
+
+export const givenATextWithoutCheckResult = (
+  adapter: AdapterInterface,
+  adapterSpec: AdapterTestSetup,
+  text: string,
+  dummyCheckId: string,
+  callback: (initialExtractedContent: string) => void,
+) => {
+  adapterSpec.setEditorContent(text);
+  adapter.registerCheckCall({ checkId: dummyCheckId });
+  const contentExtractionResult = adapter.extractContentForCheck({}) as SuccessfulContentExtractionResult;
+  callback(contentExtractionResult.content);
+};
+
+export const normalizeResultHtml = (html: string) => {
+  return html.replace(/\n|<span><\/span>/g, '');
+};
