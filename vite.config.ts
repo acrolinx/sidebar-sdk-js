7432bfd474061c712db0d2a7d964a204e45629c3
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2025-03-25 15:50:57 +0100

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: './src/index.ts',
       name: 'index',
       fileName: 'index',
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['es'],
     },
   },
   test: {
