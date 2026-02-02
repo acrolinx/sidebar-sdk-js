7043c55f83bc5e91cec787b2bda3905b401cbbbe
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2026-02-02 14:10:17 +0100

    update copyright

--- tests/browser/heretto-adapter.test.ts
+++ tests/browser/heretto-adapter.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025-present Acrolinx GmbH
+ * Copyright 2026-present Markup AI, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
